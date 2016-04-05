105b8fcd16eb17d9dd295f82aff05b05905a220a
ProfOak <OpenProfOak@gmail.com>
2016-04-05 02:40:17 -0500

    skeleton switch for opcodes, rearranged comments in types

--- spc700/types.go
+++ spc700/types.go
@@ -1,22 +1,42 @@
 package spc700
 
 type SPC_file struct {
-	Size      int
 	Headers   map[string][]byte
 	Registers map[string][]byte
 	Song      map[string][]byte
 	Ram       map[string][]byte
 }
 
+type Registers struct {
+	//Mneumonic    Desc            Control
+	F0 byte // Undocumented         ?/W
+	F1 byte // Control Register      /W
+	F2 byte // DSP Register Address R/W
+	F3 byte // DSP Register Data    R/W
+	F4 byte // Port 0               R/W
+	F5 byte // Port 1               R/W
+	F6 byte // Port 2               R/W
+	F7 byte // Port 3               R/W
+	F8 byte // Regular Memory       R/W
+	F9 byte // Regular Memory       R/W
+	FA byte // Timer-0               /W
+	FB byte // Timer-1               /W
+	FC byte // Timer-2               /W
+	FD byte // Counter-0            R/
+	FE byte // Counter-1            R/
+	FF byte // Counter-2            R/
+}
+
 type SPC700_freeze struct {
 	// Sound emulation data
-	PC [2]byte // program counter
-	A  byte    // accumulator
-	X  byte    // index register
-	Y  byte    // index register
-	SP byte    // stack pointer
-	// Program Status Word
-	PSW byte // register contains various bits that effect operation of the CPU
+	PC  [2]byte // program counter
+	A   byte    // accumulator
+	X   byte    // index register
+	Y   byte    // index register
+	SP  byte    // stack pointer
+	PSW byte    // Program Status Word (explained below)
+	DSP [128]byte
+	RAM [0x10000]byte
 	/*
 		=== RAM LAYOUT ===
 
@@ -28,9 +48,20 @@ type SPC700_freeze struct {
 		0xFFC0 - 0xFFFF MEMORY [WRITE ONLY]
 		0xFFC0 - 0xFFFF 64 BYTE IPL ROM [READ ONLY]
 	*/
-	RAM [0x10000]byte
-	DSP [128]byte
+
 }
+
+// === PSW layout ===
+// bitmasks for PSW
+// [N, V, P, B, H, I, Z, C]
+const NEGATIVE = 128      // N (Negative)
+const OVERFLOW = 64       // V (Overflow)
+const DIRECTPAGE = 32     // P (Direct page)
+const BREAK = 16          // B (Break)
+const HALFCARRY = 8       // H (Half Carry)
+const INTERUPTENABLED = 4 // I (Interrupt enabled (unused))
+const ZERO = 2            // Z (Zero)
+const CARRY = 1           // C (Carry)
 
 type CPU struct {
 	// CPU register set
